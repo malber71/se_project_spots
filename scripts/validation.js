bf76033aa10101f3941b25ce333e154a025e0943
Michael Alber <michaelalber71@gmail.com>
2025-05-09 00:32:58 -0700

    begin validation scripts

---
+++ scripts/validation.js
@@ -0,0 +1,39 @@
+const showInputError = (formEl, inputEl, errorMsg) => {
+  const errorMsgEl = document.querySelector(`#${inputEl.id}-error`);
+  errorMsgEl.textContent = errorMsg;
+};
+
+const hideInputError = (formEl, inputEl) => {
+  const errorMsgEl = document.querySelector(`#${inputEl.id}-error`);
+  errorMsgEl.textContent = "";
+};
+
+const checkInputValidity = (formEl, inputEl) => {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, inputEl.validationMessage);
+  } else {
+    hideInputError(formEl, inputEl);
+  }
+};
+
+const setEventListeners = (formEl) => {
+  const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
+  const buttonElement = formEl.querySelector(".modal__submit-btn");
+  // toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formEl, inputElement);
+      //toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = document.querySelectorAll(".modal__form");
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
+};
+
+enableValidation();
